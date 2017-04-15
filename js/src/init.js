5e1b0ea4847e6afbf17e707c1e7e2ae4288e6afa
Zeusw <meteorshield@gmail.com>
2017-04-15 14:28:35 +0800

    修改分类和背景动画的显示
    修改分类和背景动画的显示

--- js/src/init.js
+++ js/src/init.js
@@ -7,6 +7,10 @@ var closebackgrounds = [
     "/small/",
     "/small/time/index.html",
     "/small/AlphaGo/index.html",
+    "/categories/",
+    "/categories/%E5%AD%A6%E4%B9%A0%E8%B7%AF%E7%BA%BF/",
+    "/categories/%E5%8D%9A%E5%AE%A2/",
+    "/tags/"
 ]
 
 // 哪个页面开启背景动画，默认开启
@@ -19,6 +23,8 @@ var mypays = [
     "/small/",
     "/small/time/index.html",
     "/small/AlphaGo/index.html",
+    "/tags/",
+    "/categories/"
 ]
 
 for(var i = 0; i <= closebackgrounds.length; i++){
